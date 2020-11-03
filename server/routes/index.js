7d386494dc8b981540bfc63acd878b94c359c72f
Basia <b.kumor@yahoo.com>
2020-11-03 13:43:02 +0100

    create, update todos

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,48 @@
 const router = require('express').Router();
 const db = require('../db');
 
+router.get('/todos', async (req, res) => {
+    try {
+        const results = await db.query("SELECT * FROM todo", null);
+        res.status(200).json({
+            status: "success",
+            data: results.rows
+        })
+    } catch (err) {
+        alert(err.message);
+    }
+});
+
+router.get('/todos/:id', async (req, res) => {
+    const values = [req.params.id];
+    const text = "SELECT * FROM todo WHERE todo_id=$1";
+    try {
+        const results = await db.query(text, values);
+        res.status(200).json({
+            status: "success",
+            data: results.rows
+        })
+    } catch (err) {
+        console.log(err.messagge);
+    }
+});
+
+router.put('/todos/:id', async (req, res) => {
+    const {description} = req.body;
+    const {id} = req.params;
+    const values = [id, description];
+    const text = "UPDATE todo SET description=$2 WHERE todo_id=$1 RETURNING *";
+    try {
+        const results = await db.query(text, values);
+        res.status(201).json({
+            status: "success",
+            data: results.rows[0]
+        })
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
 router.post('/todos', async (req, res) => {
     const {description} = req.body;
     const values = [description];
@@ -11,7 +53,6 @@ router.post('/todos', async (req, res) => {
             status: "success",
             data: results.rows[0]
         })
-
     } catch (err) {
         alert(err.message);
     }
